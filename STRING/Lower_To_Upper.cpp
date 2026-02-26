9b4d4e99c2018fda7edd6e0d5fdbdbc839426d0b
raaaj1903 <rj471529@gmail.com>
2026-02-26 21:05:52 +0530

    add

---
+++ STRING/Lower_To_Upper.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string str;
+
+    cout << "Enter string: ";
+    getline(cin, str);
+
+    for(int i = 0; i < str.length(); i++) {
+        // check if character is lowercase
+        if(str[i] >= 'a' && str[i] <= 'z') {
+            str[i] = str[i] - 32;  // convert to uppercase
+        }
+    }
+
+    cout << "Uppercase string: " << str;
+
+    return 0;
+}
