3ac97276c17c4a756e593c8a9b5154e144c7d795
raaaj1903 <rj471529@gmail.com>
2026-03-08 22:49:58 +0530

    add

---
+++ BASIC QUESTION/fibonaci.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+void fibonacci(int n){
+    int a = 0, b = 1, next;
+
+    for(int i = 0; i < n; i++){
+        cout << a << " ";
+        next = a + b;
+        a = b;
+        b = next;
+    }
+}
+
+int main(){
+    int n;
+    cin >> n;
+
+    fibonacci(n);
+
+    return 0;
+}
