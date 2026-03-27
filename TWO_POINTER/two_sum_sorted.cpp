d02bc525f38dd6de0142822980b1ab293a9e4387
raaaj1903 <rj471529@gmail.com>
2026-03-27 16:43:55 +0530

    solved

---
+++ TWO_POINTER/two_sum_sorted.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n ,target;
+    cout<<"Enter Size Of Araay:";
+    cin>>n;
+
+    int arr[n];
+    cout<<"Enter Elements :";
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    
+    cout<<"Enter Target Sum";
+    cin >> target;
+
+    int left=0; int right = n-1;
+
+    while(left < right){
+        int sum =arr[left]+arr[right];
+        if(sum==target){
+            cout<<"Pair Founded"<<arr[left]<<arr[right];
+            return 0;
+        }
+
+        else if(sum < target) {
+        left++; 
+        }
+
+         else {
+           right--;  
+          }
+    }
+      cout << "No pair found";
+}
